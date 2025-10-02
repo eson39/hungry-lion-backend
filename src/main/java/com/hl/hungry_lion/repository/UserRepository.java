689db1fadaf03353370831bd1a59208d1c24f80d
eson39 <eson39@gatech.edu>
2025-10-02 19:13:57 -0400

    added user
    user field added

---
+++ src/main/java/com/hl/hungry_lion/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hl.hungry_lion.repository;
+
+import com.hl.hungry_lion.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
